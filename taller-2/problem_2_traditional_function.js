a6f3c86a890ffe9a50243dfc453081b071b23b2c
Johan Alexander Londoño Bedoya <johannlondonob@gmail.com>
2021-09-06 18:15:32 -0500

    added solutions of problem 2 with traditional and arrow function

---
+++ taller-2/problem_2_traditional_function.js
@@ -0,0 +1,11 @@
+function stealBlueprints( idBlueprint, detach ) {
+    if (idBlueprint > 0 && idBlueprint <= 10) {
+        console.log( `Nos robamos los planos correctos` )
+        detach( null )
+    } else {
+        console.log( 'Accedimos al plano falso' )
+        detach( 'error' )
+    }
+}
+
+stealBlueprints( 10, error => error ? console.log( 'Volver al sitio' ) : console.log( 'Despegando nave' ) )
